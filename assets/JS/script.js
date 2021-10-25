e0b0b614098bd8c781e104bfcfd592547c91f5a5
LucaVerth <luca.verthuy@gmail.com>
2021-10-25 16:06:08 +0200

    added style to css

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -10,6 +10,8 @@ for (let i = 1; i <= 100; i++) {
   el2.innerHTML = i; // Box = loop i
   el.append(el2);
 
+  console.log(el2);
+
   if (!(i % 3) && !(i % 5)) {
     el2.classList.add("multboth");
     el2.innerHTML = "fizzbuzz";
@@ -20,4 +22,4 @@ for (let i = 1; i <= 100; i++) {
     el2.classList.add("mult5");
     el2.innerHTML = "buzz";
   }
-}
+};
